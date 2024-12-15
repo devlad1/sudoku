4919513f78af3a3de89b49698994447cf707e6a7
vlad <vlad@lad>
2024-12-15 21:36:18 +0200

    tmp

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,17 @@
-import { generate } from '@pdfme/generator';
-import { image } from '@pdfme/schemas';
 import './style.css'
 
-import { Template, BLANK_PDF } from '@pdfme/common';
-
-import { getSudoku } from 'sudoku-gen';
 import { Difficulty } from 'sudoku-gen/dist/types/difficulty.type';
 
-import puzzleSchemas from '../resources/puzzleSchemas.json'
-import { createSudokuPNG } from './sudoku-drawer';
 import { NumberOfSudokus } from './types';
-import { PDFDocument } from 'pdf-lib';
+import { Settings } from './settings';
+import { SudokuPDFMaker } from './sudoku-pdf-maker';
+import { Subject } from 'rxjs';
 
 const ERROR_ELEMENT = document.getElementById("errorMessage") as HTMLParagraphElement;
-const PROGRESS_ELEMENT = document.getElementById("progress") as HTMLParagraphElement;
+const PROGRESS_ELEMENT = document.getElementById("progress-bar") as HTMLDivElement;
 
-let difficulty: Difficulty = 'medium'
-
-let numberOfSudokus: NumberOfSudokus = '6'
-let template: Template = {
-  basePdf: BLANK_PDF,
-  schemas: puzzleSchemas.sixPuzzleSchema.schemas,
-};
-
-let numPages = 1;
-
-const plugins = {
-  image
-}
+const config = new Settings();
+const sudokuMaker = new SudokuPDFMaker(config.numberOfSudokus);
 
 document.addEventListener("DOMContentLoaded", () => {  
   const difficultyOptions: { value: Difficulty; text: string }[] = [
@@ -44,9 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
     difficultyDropdown.appendChild(opt);
   });
   difficultyDropdown.addEventListener('change', () => {
-    difficulty = difficultyDropdown.value as Difficulty;
+    config.difficulty = difficultyDropdown.value as Difficulty;
   });
-  difficultyDropdown.value = difficulty
+  difficultyDropdown.value = config.difficulty
   
   const numberOptions: { value: NumberOfSudokus; text: NumberOfSudokus }[] = [
     { value: '4', text: '4' },
@@ -60,21 +44,18 @@ document.addEventListener("DOMContentLoaded", () => {
     numberOfSudokusDropdown.appendChild(opt);
   });
   numberOfSudokusDropdown.addEventListener('change', () => {
-    numberOfSudokus = numberOfSudokusDropdown.value as NumberOfSudokus
-    switch (numberOfSudokus) {
-      case '4': template.schemas = puzzleSchemas.fourPuzzleSchema.schemas; break;
-      case '6': template.schemas = puzzleSchemas.sixPuzzleSchema.schemas; break;
-    }
+    config.numberOfSudokus = numberOfSudokusDropdown.value as NumberOfSudokus
+    sudokuMaker.numberOfSudokus = config.numberOfSudokus
   });
-  numberOfSudokusDropdown.value = numberOfSudokus
+  numberOfSudokusDropdown.value = config.numberOfSudokus
 
   function handleInputChange(event: Event): void {
     const target = event.target as HTMLInputElement;
 
-    numPages = Number(target.value)
+    config.numPages = Number(target.value)
   }
   const numPagesInput = document.getElementById('numberOfPages') as HTMLInputElement;
-  numPagesInput.value = numPages.toString()
+  numPagesInput.value = config.numPages.toString()
   numPagesInput.addEventListener('input', handleInputChange);
 
   const button = document.getElementById("generate") as HTMLButtonElement;
@@ -87,35 +68,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function generatePdf(): Promise<void> {  
-  if (isNaN(numPages)) {
+  if (isNaN(config.numPages)) {
     ERROR_ELEMENT.textContent = 'Количество запрошенных вами страниц не является числом.'
-  } else if (numPages <= 0) {
+  } else if (config.numPages <= 0) {
     ERROR_ELEMENT.textContent = 'Количество страниц должно быть положительным.'
-  } else if (numPages > 50) {
+  } else if (config.numPages > 50) {
     ERROR_ELEMENT.textContent = 'Количество страниц должно быть менее 50.'
   } else {
-    let pdfDataArray: Promise<Uint8Array>[] = new Array(numPages)
-    let progress = 0;
-    const progressBar = document.getElementById("progress-bar") as HTMLDivElement;
 
-    for (let i = 0; i < numPages; i++) {
-      let puzzleSchemas = template.schemas[0]
-      for (let puzzle of puzzleSchemas) {
-        const sudoku = getSudoku(difficulty);
-        puzzle.content = createSudokuPNG(sudoku);
-      }
-  
-      pdfDataArray[i] = generate({ template: template, inputs: [{}], plugins: plugins })
-    }
-
-    const mergedPdf = await PDFDocument.create();
-    for (const pdfData of pdfDataArray) {
-      const currentPdf = await PDFDocument.load(await pdfData);
-      const copiedPages = await mergedPdf.copyPages(currentPdf, currentPdf.getPageIndices());
-      copiedPages.forEach((page) => {
-        mergedPdf.addPage(page);
-      });
-    }
+    const progressSubject = new Subject<number>()
+    progressSubject.subscribe((progress: number) => {
+      console.log(progress)
+      PROGRESS_ELEMENT.style.width = `${100 * progress}%`
+    })
+    const mergedPdf = await sudokuMaker.make(config, progressSubject)
 
     const mergedPdfBytes = await mergedPdf.save();
     const mergedPdfBlob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
